2b20d5fbcf807e684927aa752eda05923cf532bb
marouan <marouan.benmansour@symdrik.com>
2019-12-18 23:51:48 +0100

    tp 2 angular

--- my-project/src/app/app.component.spec.ts
+++ my-project/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-project'`, () => {
+  it(`should have as title 'tp1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-project');
+    expect(app.title).toEqual('tp1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tp1 app is running!');
   });
 });
